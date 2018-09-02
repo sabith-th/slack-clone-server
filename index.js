4958e4208ba1dbe4d0005d59cc2a744a75e0771a
SabithHaneef <sabith.haneef@target.com>
2018-09-02 14:25:01 +0530

    setup basic graphql server and setup database models

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { ApolloServer, makeExecutableSchema } from 'apollo-server-express';
+import typeDefs from './schema';
+import resolvers from './resolver';
+import models from './models';
+
+const PORT = 8080;
+const app = express();
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app });
+
+models.sequelize.sync().then(() => {
+  app.listen({ port: PORT });
+});
