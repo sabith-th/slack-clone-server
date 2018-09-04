c4b6164942c8b0d3993ea4ed54990cc55982a412
SabithHaneef <sabith.th.07@gmail.com>
2018-09-04 22:41:08 +0530

    added jwt verification for logged in users, refreshToken for logged in users, refactored formatErrors, made logged in user owner of created team

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import { ApolloServer, makeExecutableSchema } from 'apollo-server-express';
 import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import cors from 'cors';
 
 import models from './models';
+import { refreshTokens } from './auth';
 
 const SECRET = 'asongoficeandfire';
 const SECRET2 = 'agameofthrones';
@@ -15,6 +17,29 @@ const resolvers = mergeResolvers(fileLoader(path.join(__dirname, 'resolvers')));
 const PORT = 8080;
 const app = express();
 app.use(cors('*'));
+
+const addUser = async (req, res, next) => {
+  const token = req.headers['x-token'];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (err) {
+      const refreshToken = req.headers['x-refresh-token'];
+      const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+      if (newTokens.token && newTokens.refreshToken) {
+        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
+        res.set('x-token', newTokens.token);
+        res.set('x-refresh-token', newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
+
+app.use(addUser);
+
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
@@ -22,14 +47,12 @@ const schema = makeExecutableSchema({
 
 const server = new ApolloServer({
   schema,
-  context: {
+  context: ({ req }) => ({
     models,
-    user: {
-      id: 1,
-    },
+    user: req.user,
     SECRET,
     SECRET2,
-  },
+  }),
 });
 server.applyMiddleware({ app });
 
