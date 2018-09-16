07582ba753334daf22ba096369b68f2dfc872023
SabithHaneef <sabith.th.07@gmail.com>
2018-09-16 23:48:28 +0530

    add requiresTeamAccess permission check (note: it is not enforced currently as apollo-server has issues which prevents context being appended with return values from onConnect in Subscription

--- permissions.js
+++ permissions.js
@@ -15,3 +15,17 @@ export default createResolver((parent, args, { user }) => {
     throw new Error('Not authenticated');
   }
 });
+
+export const requiresTeamAccess = createResolver(async (parent, { channelId }, context) => {
+  const { models, user } = context;
+  if (!user || !user.id) {
+    throw new Error('Not authenticated');
+  }
+  const channel = await models.Channel.findOne({ where: { id: channelId } });
+  const member = await models.Member.findOne({
+    where: { teamId: channel.teamId, userId: user.id },
+  });
+  if (!member) {
+    throw new Error('Only team members can subscribe to channel messages');
+  }
+});
